b0a22a0c89abe39050d34a9c79c4b608bb3bdb12
Your Name <you@example.com>
2020-07-24 10:14:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import itachi from "./itachi.jpeg"
 import './App.css';
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+       <h1 className="title red">Your name here</h1>
+
+      <br/>
+
+      <img src={itachi} width="320" height="240"/>
+
+      <br />
+
+      <img src="/sasuke.jpeg" width="320" height="240" />
+
     </div>
+
+      <video width="320" height="240" controls>
+
+        <source src="/bird.mp4" type="video/mp4" />
+
+      </video>
+</div>
   );
 }
 
